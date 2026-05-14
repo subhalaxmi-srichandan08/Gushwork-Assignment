3ec96a38f1dfc6cd5690c531dbd731607f881e82
subhalaxmi-srichandan08 <subhalaxmisrichandan08@gmail.com>
2026-05-14 07:15:13 +0530

    feat hoem page

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch("./pages/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-container").innerHTML = data;
+    console.log("Navbar Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading navbar:", error);
+  });
+
+
+// LOAD PATH
+fetch("./pages/path.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("path-container").innerHTML = data;
+    console.log("Path Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading Path:", error);
+  });
+
+
+// LOAD HOME
+fetch("./pages/home.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("home-container").innerHTML = data;
+    console.log("Home Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading home:", error);
+  });
