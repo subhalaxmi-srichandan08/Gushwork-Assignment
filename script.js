e72b6b77281eaa73d8bca2658db3762b72b2df7d
subhalaxmi-srichandan08 <subhalaxmisrichandan08@gmail.com>
2026-05-15 07:16:36 +0530

    whole assignment completed

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ fetch("./pages/home.html")
     console.log("Error loading home:", error);
   });
 
-  // LOAD HOME
+// LOAD HOME
 fetch("./pages/tech.html")
   .then((response) => response.text())
   .then((data) => {
@@ -43,7 +43,7 @@ fetch("./pages/tech.html")
     console.log("Error loading tech:", error);
   });
 
-  fetch("./pages/requestquote.html")
+fetch("./pages/requestquote.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("request-container").innerHTML = data;
@@ -51,4 +51,65 @@ fetch("./pages/tech.html")
   })
   .catch((error) => {
     console.log("Error loading Request for quote:", error);
+  });
+
+fetch("./pages/faq.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("faq-container").innerHTML = data;
+    console.log("faq Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading faq:", error);
+  });
+
+fetch("./pages/versatileapplication.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("versatile-container").innerHTML = data;
+    console.log("versatile Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading versatile:", error);
+  });
+
+fetch("./pages/process-section.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("process-container").innerHTML = data;
+    console.log("process Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading process:", error);
+  });
+
+
+fetch("./pages/tp.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("trusted-container").innerHTML = data;
+    console.log("process Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading process:", error);
+  });
+
+fetch("./pages/piping-solution.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("piping-container").innerHTML = data;
+    console.log("piping-solution Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading process:", error);
+  });
+
+  fetch("./pages/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer-container").innerHTML = data;
+    console.log("footer-container Loaded Successfully");
+  })
+  .catch((error) => {
+    console.log("Error loading process:", error);
   });
